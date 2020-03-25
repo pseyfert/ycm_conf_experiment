55c6c8e7e29ace7423f31265593b99528165c1f2
Paul Seyfert <pseyfert@cern.ch>
2020-03-25 12:30:07 +0100

    backwards compatibility for lxplus6 (python 2.6)

--- ycm_conf_utils/lhcb.py
+++ ycm_conf_utils/lhcb.py
@@ -19,7 +19,68 @@
 # or submit itself to any jurisdiction.
 
 import os
-from subprocess import check_output, CalledProcessError
+try:
+    from subprocess import check_output, CalledProcessError
+except ImportError:
+    # This section section taken from pep8radius
+    # Copyright (c) [2014] [Andy Hayden]
+    #
+    # Permission is hereby granted, free of charge, to any person obtaining a copy of
+    # this software and associated documentation files (the "Software"), to deal in
+    # the Software without restriction, including without limitation the rights to
+    # use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+    # the Software, and to permit persons to whom the Software is furnished to do so,
+    # subject to the following conditions:
+    #
+    # The above copyright notice and this permission notice shall be included in all
+    # copies or substantial portions of the Software.
+    #
+    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+    # FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+    # COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+    # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+    # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+    try:
+        from subprocess import STDOUT, check_output, CalledProcessError
+    except ImportError:  # pragma: no cover
+        # python 2.6 doesn't include check_output
+        # monkey patch it in!
+        import subprocess
+        STDOUT = subprocess.STDOUT
+
+        def check_output(*popenargs, **kwargs):
+            if 'stdout' in kwargs:  # pragma: no cover
+                raise ValueError('stdout argument not allowed, '
+                                 'it will be overridden.')
+            process = subprocess.Popen(stdout=subprocess.PIPE,
+                                       *popenargs, **kwargs)
+            output, _ = process.communicate()
+            retcode = process.poll()
+            if retcode:
+                cmd = kwargs.get("args")
+                if cmd is None:
+                    cmd = popenargs[0]
+                raise subprocess.CalledProcessError(retcode, cmd,
+                                                    output=output)
+            return output
+        subprocess.check_output = check_output
+
+        # overwrite CalledProcessError due to `output`
+        # keyword not being available (in 2.6)
+        class CalledProcessError(Exception):
+
+            def __init__(self, returncode, cmd, output=None):
+                self.returncode = returncode
+                self.cmd = cmd
+                self.output = output
+
+            def __str__(self):
+                return "Command '%s' returned non-zero exit status %d" % (
+                    self.cmd, self.returncode)
+        subprocess.CalledProcessError = CalledProcessError
+
 import glob
 import stat
 import time
@@ -38,13 +99,12 @@ def getdb(filename, logger=None):
                 common = os.path.dirname(common)
         if logger is not None:
             logger.debug("git repository root at: {}".format(common))
-        ccs = {
-            os.stat(f)[stat.ST_MTIME]: f
-            for f in glob.glob(os.path.join(
+        ccs = {}
+        for f in glob.glob(os.path.join(
                 common,
                 b"build.*",
-                b"compile_commands.json"))
-        }
+                b"compile_commands.json")):
+            ccs[os.stat(f)[stat.ST_MTIME]] = f
         if logger is not None:
             logger.debug("found compile_commands.json at: {}".format(ccs))
         # if several compile_commands data bases are found, pick the newest:
